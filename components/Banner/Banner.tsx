b8e874e1b25c5a0fb37c7c7b3f3250b3b4ac6e70
Santhosh Raju <santhosh.codeholic@gmail.com>
2021-06-01 18:22:38 +0530

    Redesign of header and banner implementation

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link'
 
 export const Banner = () => {
   return (
     <div className="site-banner">
-      <div className="site-banner__hero-info">
-        <h1>Build. Blog. Share</h1>
-        <p>
-          Sharing my experience of being a Dev via blogs and bit of my life
-          experiences.
-        </p>
+      <div className="site-banner__container">
+        <div className="site-banner__container--hero-info">
+          <h1>Build. Blog. Share</h1>
+          <p>
+            A developer’s personal blogging site, where I blog about my
+            experiences as developer and living a life.
+          </p>
+        </div>
+        <div className="site-banner__container--profile">
+          <div className="site-banner__container--profile-image">
+            <img src="/site-assets/Me.png" alt="Me profile" />
+            <div className="bg-box"></div>
+          </div>
+          <div className="site-banner__container--profile-action">
+            <Link href="/about">
+              <>
+                <h5>Say Hello</h5>
+                <img
+                  src="/site-assets/hello.svg"
+                  alt="Say hello"
+                  width="40"
+                  height="40"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="site-banner__divider"></div>
     </div>
   )
 }
