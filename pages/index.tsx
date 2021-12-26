a312ba45431490f10c1fa1b2728be5ced632e6a2
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2021-12-26 17:10:53 +0530

    Footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { Card } from "../components/Card";
 import { Home } from "./Home";
+import { Footer } from "../components/Footer";
 
 function Index() {
   return (
     <Box>
       <Home />
+      <Footer />
     </Box>
   );
 }
