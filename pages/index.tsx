67460778397434039fd1403d86686b19b0defcb5
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2021-12-27 08:30:31 +0530

    Adding favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
+import Head from "next/head";
 import { Box } from "@chakra-ui/react";
-import { Card } from "../components/Card";
 import { Home } from "./Home";
 import { Footer } from "../components/Footer";
 
 function Index() {
   return (
-    <Box>
-      <Home />
-      <Footer />
-    </Box>
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico"></link>
+        <title>santhoshraju.dev</title>
+      </Head>
+      <Box>
+        <Home />
+        <Footer />
+      </Box>
+    </div>
   );
 }
 
