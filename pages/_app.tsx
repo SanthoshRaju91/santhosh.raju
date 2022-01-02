612d59af7a9b64dc417681cf71257ce064355a33
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2022-01-02 19:12:22 +0530

    changing font family to Open Sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/800.css";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../theme";
