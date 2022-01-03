3dae5ed76c6d5c57e9b203b598943dfd13867c53
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2022-01-03 17:14:24 +0530

    Changing layout of Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,7 +5,7 @@ import LogoLarge from "../../assets/Logo-Large.svg";
 
 const Header: React.FC = () => {
   return (
-    <Box my={8}>
+    <Box maxW={"80%"} w={"85%"} m={"auto"} my={8}>
       <HStack>
         <Link href={"/"} passHref>
           <Image src={LogoLarge} alt="Logo" width={300} height={75}></Image>
