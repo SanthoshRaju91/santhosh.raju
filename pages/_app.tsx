c95ad954fdf3fd46a7d9052b982fa23b00b94b96
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2022-01-03 17:12:31 +0530

    Moving metadata Head to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,17 @@ import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/500.css";
 import "@fontsource/open-sans/800.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../theme";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico"></link>
+        <title>santhoshraju.dev</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,13 @@
-import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { Home } from "./Home";
 import { Footer } from "../components/Footer";
 
 function Index() {
   return (
-    <div>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico"></link>
-        <title>santhoshraju.dev</title>
-      </Head>
-      <Box>
-        <Home />
-        <Footer />
-      </Box>
-    </div>
+    <Box>
+      <Home />
+      <Footer />
+    </Box>
   );
 }
 
