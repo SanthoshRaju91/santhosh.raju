ba822f5d7c0fac4c0f375bb6493340aefa1a0e7f
SanthoshRaju91 <santhosh.codeholic@gmail.com>
2022-01-15 12:48:51 +0530

    Removing Box Layouts for all pages

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -8,9 +8,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
-import matter from "gray-matter";
 import { Card } from "../components/Card";
-import { Footer } from "../components/Footer";
 import { Tag } from "../components/Tag";
 import { BlogMatter } from "../common/type";
 import getBlogs from "../common/getBlogs";
@@ -21,94 +19,91 @@ export type BlogsProps = {
 
 const Blogs: React.FC<BlogsProps> = ({ blogs }) => {
   return (
-    <Box>
-      <Box maxW={"80%"} w={"85%"} m={"auto"} mt={24}>
-        <Heading
-          fontSize={"2xl"}
-          fontWeight={"black"}
-          textColor={"blackAlpha.800"}
-          pb={1}
-          inlineSize={"max-content"}
-          borderBottomWidth={1}
-          borderBottomColor={"blackAlpha.700"}
-        >
-          All Blogs
-        </Heading>
-        <Flex
-          mt={12}
-          gap={12}
-          flexDirection={{ sm: "column", md: "column", lg: "row" }}
-        >
-          <Box w={{ sm: "100%", md: "100%", lg: "70%" }}>
-            <VStack spacing={4}>
-              {blogs.map((blog, index) => (
-                <Card
-                  key={index}
-                  title={blog.title}
-                  synopsis={blog.synopsis}
-                  user={blog.author}
-                  userAvatar={blog.authorProfilePic}
-                  published={blog.published}
-                  tags={blog.tags}
-                  slug={blog.slug}
-                />
-              ))}
-            </VStack>
-            <HStack mt={6}>
-              <Button
-                bgColor={"transparent"}
-                fontSize={"lg"}
-                fontWeight={"bold"}
-                iconSpacing={2}
-                textColor={"blackAlpha.700"}
-                _hover={{ bgColor: "transparent", textColor: "blackAlpha.800" }}
-                leftIcon={<AiOutlineArrowLeft size={"20px"} />}
-              >
-                Previous
-              </Button>
-              <Button
-                bgColor={"transparent"}
-                fontSize={"lg"}
-                fontWeight={"bold"}
-                iconSpacing={2}
-                textColor={"blackAlpha.700"}
-                _hover={{ bgColor: "transparent", textColor: "blackAlpha.800" }}
-                rightIcon={<AiOutlineArrowRight size={"20px"} />}
-              >
-                Next
-              </Button>
-            </HStack>
-          </Box>
-
-          <Box
-            w={{ sm: 0, md: 0, lg: "30%" }}
-            display={{ sm: "none", md: "none", lg: "block" }}
-          >
-            <Heading
+    <>
+      <Heading
+        fontSize={"2xl"}
+        fontWeight={"black"}
+        textColor={"blackAlpha.800"}
+        pb={1}
+        inlineSize={"max-content"}
+        borderBottomWidth={1}
+        borderBottomColor={"blackAlpha.700"}
+      >
+        All Blogs
+      </Heading>
+      <Flex
+        mt={12}
+        gap={12}
+        flexDirection={{ sm: "column", md: "column", lg: "row" }}
+      >
+        <Box w={{ sm: "100%", md: "100%", lg: "70%" }}>
+          <VStack spacing={4}>
+            {blogs.map((blog, index) => (
+              <Card
+                key={index}
+                title={blog.title}
+                synopsis={blog.synopsis}
+                user={blog.author}
+                userAvatar={blog.authorProfilePic}
+                published={blog.published}
+                tags={blog.tags}
+                slug={blog.slug}
+              />
+            ))}
+          </VStack>
+          <HStack mt={6}>
+            <Button
+              bgColor={"transparent"}
               fontSize={"lg"}
-              fontWeight={"black"}
-              textColor={"blackAlpha.800"}
-              pb={1}
-              inlineSize={"max-content"}
-              borderBottomWidth={1}
-              borderBottomColor={"blackAlpha.800"}
+              fontWeight={"bold"}
+              iconSpacing={2}
+              textColor={"blackAlpha.700"}
+              _hover={{ bgColor: "transparent", textColor: "blackAlpha.800" }}
+              leftIcon={<AiOutlineArrowLeft size={"20px"} />}
             >
-              Tags
-            </Heading>
-            <Grid mt={4} templateColumns={"repeat(2, 1fr)"} gap={4}>
-              <Tag text="Javascript" />
-              <Tag text="Frontend" />
-              <Tag text="Ansible" />
-              <Tag text="React.js" />
-              <Tag text="Devops" />
-              <Tag text="Walk of Life" />
-              <Tag text="Books" />
-            </Grid>
-          </Box>
-        </Flex>
-      </Box>
-      <Footer />
-    </Box>
+              Previous
+            </Button>
+            <Button
+              bgColor={"transparent"}
+              fontSize={"lg"}
+              fontWeight={"bold"}
+              iconSpacing={2}
+              textColor={"blackAlpha.700"}
+              _hover={{ bgColor: "transparent", textColor: "blackAlpha.800" }}
+              rightIcon={<AiOutlineArrowRight size={"20px"} />}
+            >
+              Next
+            </Button>
+          </HStack>
+        </Box>
+
+        <Box
+          w={{ sm: 0, md: 0, lg: "30%" }}
+          display={{ sm: "none", md: "none", lg: "block" }}
+        >
+          <Heading
+            fontSize={"lg"}
+            fontWeight={"black"}
+            textColor={"blackAlpha.800"}
+            pb={1}
+            inlineSize={"max-content"}
+            borderBottomWidth={1}
+            borderBottomColor={"blackAlpha.800"}
+          >
+            Tags
+          </Heading>
+          <Grid mt={4} templateColumns={"repeat(2, 1fr)"} gap={4}>
+            <Tag text="Javascript" />
+            <Tag text="Frontend" />
+            <Tag text="Ansible" />
+            <Tag text="React.js" />
+            <Tag text="Devops" />
+            <Tag text="Walk of Life" />
+            <Tag text="Books" />
+          </Grid>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
