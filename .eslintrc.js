0cef0259512529ade1aaae27b353921f60708d7e
SanthoshRaju91 <santhoshraju.ai@gmail.com>
2023-05-28 10:02:08 +0530

    feat: add linter and formatter
    Adding linter and formatter for checking the code quality

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:astro/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};
